87e7f8f2da66c812117c5ebc401d3e8a9ac7216b
matsujirushi <matsujirushi@live.jp>
2014-07-09 11:27:11 +0900

    ・使う関数を、APIからWinRTに方針変更。 ・ライブラリとコンソールアプリを用意。

---
+++ mjwinble/MjGattDeviceService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matsujirushi.mjwinble
+{
+    public class MjGattDeviceService
+    {
+        /// <summary>
+        /// BluetoothLEデバイスのInterfaceClassGuid
+        /// </summary>
+        private static Guid GUID_BLUETOOTHLE_DEVICE_INTERFACE = new Guid("781AEE18-7733-4CE4-ADD0-91F41C67B592");
+
+        /// <summary>
+        /// GATTサービスのInterfaceClassGuid
+        /// </summary>
+        private static Guid GUID_BLUETOOTH_GATT_SERVICE_DEVICE_INTERFACE = new Guid("6E3BB679-4372-40C8-9EAA-4509DF260CD8");
+
+        /// <summary>
+        /// BluetoothLEデバイスのAQS文字列を取得します。
+        /// </summary>
+        /// <returns></returns>
+        public static string GetDeviceSelector()
+        {
+            return string.Concat("System.Devices.InterfaceClassGuid:=\"{", GUID_BLUETOOTHLE_DEVICE_INTERFACE.ToString(), "}\" AND System.Devices.InterfaceEnabled:=System.StructuredQueryType.Boolean#True");
+        }
+
+        /// <summary>
+        /// BluetoothLEデバイスのAQS文字列を取得します。
+        /// </summary>
+        /// <param name="containerId">ContainerId</param>
+        /// <returns></returns>
+        public static string GetDeviceSelector(Guid containerId)
+        {
+            return string.Concat("System.Devices.InterfaceClassGuid:=\"{", GUID_BLUETOOTHLE_DEVICE_INTERFACE.ToString(), "}\" AND System.Devices.ContainerId:=\"{", containerId.ToString(), "}\" AND System.Devices.InterfaceEnabled:=System.StructuredQueryType.Boolean#True");
+        }
+
+        /// <summary>
+        /// GATTサービスのAQS文字列を取得します。
+        /// </summary>
+        /// <returns></returns>
+        public static string GetServiceSelector()
+        {
+            return string.Concat("System.Devices.InterfaceClassGuid:=\"{", GUID_BLUETOOTH_GATT_SERVICE_DEVICE_INTERFACE.ToString(), "}\" AND System.Devices.InterfaceEnabled:=System.StructuredQueryType.Boolean#True");
+        }
+
+        /// <summary>
+        /// GATTサービスのAQS文字列を取得します。
+        /// </summary>
+        /// <param name="containerId"></param>
+        /// <returns></returns>
+        public static string GetServiceSelector(Guid containerId)
+        {
+            return string.Concat("System.Devices.InterfaceClassGuid:=\"{", GUID_BLUETOOTH_GATT_SERVICE_DEVICE_INTERFACE.ToString(), "}\" AND System.Devices.ContainerId:=\"{", containerId.ToString(), "}\" AND System.Devices.InterfaceEnabled:=System.StructuredQueryType.Boolean#True");
+        }
+
+    }
+}
---
+++ mjwinbleConsoleApp/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Text.RegularExpressions;
+
+// Windows Desktop Appから、WinRTを参照するには？
+//
+// 1. プロジェクトファイルのPropertyGroupエレメントに、"<TargetPlatformVersion>8.1</TargetPlatformVersion>"を追加します。
+// 2. プロジェクトの参照設定で、コアのWindowsをチェックします。
+// 3. プロジェクトの参照設定で、System.Runtimeを追加します。
+
+using Windows.Foundation;
+using Windows.Devices.Enumeration;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+
+using matsujirushi.mjwinble;
+
+namespace mjwinbleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // BluetoothLEデバイス一覧を表示します。
+            var devicesInfo = DeviceInformationFindAll(MjGattDeviceService.GetDeviceSelector());
+            for (int i = 0; i < devicesInfo.Count; i++)
+            {
+                Console.WriteLine("{0}. [{1}]", i + 1, devicesInfo[i].Name);
+            }
+            Console.Write("Input: ");
+            var deviceInfo = devicesInfo[int.Parse(Console.ReadLine()) - 1];
+            Console.WriteLine();
+
+            // GATTサービス一覧を表示します。
+            var servicesInfo = DeviceInformationFindAll(MjGattDeviceService.GetServiceSelector((Guid)deviceInfo.Properties["System.Devices.ContainerId"]));
+            for (int i = 0; i < servicesInfo.Count; i++)
+            {
+                var uuid = UuidFromDeviceInstanceId((string)servicesInfo[i].Properties["System.Devices.DeviceInstanceId"]);
+                Console.WriteLine("{0}. [{1}]", i + 1, uuid);
+            }
+            Console.Write("Input: ");
+            var serviceInfo = servicesInfo[int.Parse(Console.ReadLine()) - 1];
+            Console.WriteLine();
+
+            // GATTサービスインスタンスを取得します。
+            var result = GattDeviceService.FromIdAsync(serviceInfo.Id);
+            while (result.Status == AsyncStatus.Started)
+            {
+                System.Threading.Thread.Sleep(100);
+            }
+            var service = result.GetResults();
+
+            // Characteristicを取得します。
+            var characteristics = service.GetCharacteristics(GattDeviceService.ConvertShortIdToUuid(0x2A19));   // Battery Level
+
+            Console.Write("Completed: ");
+            Console.ReadKey();
+        }
+
+        private static DeviceInformationCollection DeviceInformationFindAll(string aqsFilter)
+        {
+            var result = DeviceInformation.FindAllAsync(aqsFilter, new string[] { "System.Devices.ContainerId", "System.Devices.InterfaceClassGuid" });
+            while (result.Status == AsyncStatus.Started)
+            {
+                System.Threading.Thread.Sleep(100);
+            }
+
+            return result.GetResults();
+        }
+
+        private static Guid UuidFromDeviceInstanceId(string deviceInstanceId)
+        {
+            Regex regex = new Regex(@"^BTHLEDevice\\\{(.*)\}");
+            var match = regex.Match(deviceInstanceId);
+            if (!match.Success)
+            {
+                throw new ApplicationException();
+            }
+
+            return new Guid(match.Groups[1].Value);
+        }
+
+    }
+}
